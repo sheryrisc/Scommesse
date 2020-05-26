166891c74877e7bf530707b5beff2afe0abd9659
turtleseverywhere <s.samele@libero.it>
2020-05-26 20:48:53 +0200

    Add project files.

---
+++ Default.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Scommesse
+{
+    /*
+        - creare una homepage che permetta di scegliere il gioco
+        - creare una pagina per ciascun gioco
+            - al momento solo "Indovina un numero"     
+     */
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
